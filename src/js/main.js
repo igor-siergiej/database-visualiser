cba57bd45475383a52b5d12c90310b171996fc5a
Igor Siergiej <igorsiergiej@gamil.com>
2023-02-06 01:56:48 +0000

    Implemented SQL parsing for multiple tables

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import * as bootstrap from 'bootstrap';
   
   function generateTableHead(table, data) {
+    data.shift();
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of data) {
@@ -23,33 +24,49 @@ import * as bootstrap from 'bootstrap';
   }
   
   function parseSQL(text) {
-    // need to first split by each 'CREATE TABLE'
+    var returnList = [];
     // need to check if statements are not IF NOT EXISTS and CREATE TABLE AS
-    let textArray = text.split(',');
-    textArray[0] = textArray[0].split('(')[1];
-    textArray.forEach(element => {
-        console.log(element);
+    let tableArray = text.split('CREATE TABLE'); // this should have length the number of tables
+    tableArray.shift();
+    tableArray.forEach(text => {
+        text = text.replace(/(\r\n|\n|\r)/gm, "");
+        let textArray = text.split(',');
+        let tableTitle = textArray[0].split('(')[0];
+        textArray[0] = textArray[0].split('(')[1];
+        textArray.unshift(tableTitle)
+        let lastIndex = textArray.length -1;
+        let length = textArray[lastIndex].length;
+        textArray[lastIndex] = textArray[lastIndex].substring(0,length-2);
+        returnList.push(textArray);
     });
-    let lastIndex = textArray.length -1;
-    let length = textArray[lastIndex].length;
-    textArray[lastIndex] = textArray[lastIndex].substring(0,length-2);
-    return textArray
+    
+    return returnList
   }
  
 
   document.querySelector('.click').addEventListener('click', (e) => {
-    let table = document.querySelector(".table");
-    if (table != null) {
-        table.deleteTHead();
-    }
-    let text = document.querySelector(".textarea").value;
-    let headers = parseSQL(text);
-    generateTableHead(table, headers);
+    let div = document.querySelector(".tableArea");
+     if (div.childElementCount == 0) { // need a better way to check for needing to reconstruct tables
+        let text = document.querySelector(".textarea").value;
+        let sqlText = parseSQL(text);
 
+        for (const element of sqlText) {
 
+            let heading = document.createElement("h3");
+            heading.innerText = element[0];
+            div.appendChild(heading);
+            
+            let table = document.createElement("table");
+            table.className = "table";
+            generateTableHead(table, element);
+            
+            div.appendChild(table);
+        }
+     }
+    });
+    
     //generateTable(table, data);   this should be used for adding values to table so
     // these can be used to implement correcting queries?
 
-
     //e.target.textContent = 'Clicked!';
-  });
+
