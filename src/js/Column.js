39402100f63a51f2d7f2906c9cf82bf5f85c2f8b
Igor Siergiej <igorsiergiej@gamil.com>
2023-02-13 20:57:07 +0000

    Added Column Class

---
+++ src/js/Column.js
@@ -0,0 +1,18 @@
+export default class Column {
+    name;
+    type;
+
+    constructor(inputString) {
+        console.log(inputString);
+    }
+
+    // input string e.i. table name and list of columns
+    parseString() {
+
+    }
+
+    // add a table to the div or div id?
+    createTable(div) {
+        
+    }
+}
--- src/js/Table.js
+++ src/js/Table.js
@@ -1,11 +1,32 @@
-export default class Table {
-    name;
-    columns;
-    keys;
+import jsTokens from "js-tokens";
 
-    constructor(name,columns,keys) {
-        this.name = name;
-        this.columns = columns;
-        this.keys = keys;
-    }
+export default class Table {
+	name;
+	columns;
+	keys;
+
+	constructor(inputString) {
+		const tokenizedInputString = jsTokens(inputString);
+
+		
+		for (const token of tokenizedInputString) {
+			console.log(token)
+			if (token.type == "IdentifierName") {
+				this.name = token.value;
+				break;
+			}
+		}
+		console.log(this);
+		//for the number of ',' loop over and create columns and set them as instance variables
+	}
+
+	// input string e.i. table name and list of columns
+	parseString() {
+
+	}
+
+	// add a table to the div or div id?
+	createTable(div) {
+
+	}
 }
