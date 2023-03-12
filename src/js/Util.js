94fc966b47953e5599c4dd178d5ff107cb7ba27f
Igor Siergiej <igorsiergiej@gmail.com>
2023-03-12 22:38:24 +0000

    Added arrays of datatypes to columnType And added Util class

---
+++ src/js/Util.js
@@ -0,0 +1,30 @@
+export default class Util {
+
+
+    // Name must contain only letters (a-z, A-Z), numbers (0-9), or underscores ( _ ) 
+	// Name must begin with a letter or underscore.
+	// Name must be less than the maximum length of 59 characters. 
+
+	static isNameValid(name) {
+		var valid = false
+		const validSQLColumnNameRegex = /^[A-Za-z_][A-Za-z\d_]*$/;
+
+		if (name.match(validSQLColumnNameRegex) && name.length < 59) {
+			valid = true
+		}
+		return valid
+	}
+
+    // write a function to join punctuators for a tokenized array
+    static joinPunctuators(tokenizedArray) {
+        for (var i = 0; i < tokenizedArray.length;i++) {
+            var value = tokenizedArray[i].value
+            var type = tokenizedArray[i].type
+            if (type == "Punctuator" && value != "(" && value != ")") {
+                tokenizedArray[i-1].value += tokenizedArray[i].value + tokenizedArray[i+1].value
+                tokenizedArray.splice(i,i+1)
+            }
+        }
+        console.log(tokenizedArray)
+    }
+}
