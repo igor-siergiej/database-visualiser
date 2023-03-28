ba7c2d4cddef823941d177c732cdbfa42463ce30
Igor Siergiej <igorsiergiej@gmail.com>
2023-03-28 16:49:12 +0100

    Added column constraint parsing

--- src/js/Column.js
+++ src/js/Column.js
@@ -14,9 +14,8 @@ export default class Column {
 
     constructor(tokenizedArray, columns) {
         Util.joinPunctuators(tokenizedArray)
-        for (const element of tokenizedArray) {
-            console.log(element)
-        }
+        
+
 
         // first element should be name
         var columnName = tokenizedArray[0].value
@@ -27,7 +26,6 @@ export default class Column {
         }
 
         var columnType = new ColumnType();
-
         var dataType = tokenizedArray[1].value
 
         if (tokenizedArray[2] !== undefined) { // checking if there are enough arguments to parse
@@ -35,6 +33,7 @@ export default class Column {
 
             if (openBracket == "(" && tokenizedArray[4].value == ")") { //brackets for dataType
                 columnType.setType(dataType, tokenizedArray[3].value) //set type with dataType and value
+                tokenizedArray = tokenizedArray.splice(5)
             } else if (openBracket == "(") { // open bracket is present but closed is not
                 if (tokenizedArray[3].value == ")") { // nothing in between the brackets means no value
                     throw new SyntaxError(`Empty Column Type Value`, "()")
@@ -45,14 +44,50 @@ export default class Column {
                 throw new SyntaxError(`Missing open bracket for Column Type`, dataType)
             } else {
                 columnType.setType(dataType)
+                tokenizedArray = tokenizedArray.splice(2)
             }
         } else {// if there is only columnName and dataType
-            columnType.setType(dataType)          
+            columnType.setType(dataType) 
+            tokenizedArray = tokenizedArray.splice(2)         
         }
 
-        // need to get index to start parsing constraints or go back to removing columnName and Type
-    
         this.columnType = columnType
+
+        for (const element of tokenizedArray) {
+            console.log(element)
+        }
+
+        while (tokenizedArray.length > 0) {
+            var word = tokenizedArray[0].value
+            switch (word.toUpperCase()) {
+                case "NULL":
+                    break;
+                case "NOT":
+                    if (tokenizedArray[1].value == "NULL") {
+                        this.nullable = false
+                        tokenizedArray.splice(0,2)
+                    } else {
+                        throw new SyntaxError(`Expecting NULL instead of "${tokenizedArray[1].value}"`, tokenizedArray[1].value)
+                    }
+                    break;
+                case "PRIMARY":
+                    if (tokenizedArray[1].value == "KEY") {
+                        this.addKey("P")
+                        tokenizedArray.splice(0,2)
+                    }
+                    break;
+                case "UNIQUE":
+                    this.unique = true
+                    tokenizedArray.splice(0)
+                    break;
+                    default:
+                        throw new SyntaxError(`Unrecognised Constraint: ${word}`, word)
+            }
+        }
+
+       
+
+        
         this.constraints = tokenizedArray
     }
 
